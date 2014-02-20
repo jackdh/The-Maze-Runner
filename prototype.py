ea068d1b7e8cae3b240eff8d92de7a775c0a62d9
jackdh <jackdh@hotmail.co.uk>
2014-02-20 21:29:11 +0000

    Rewrote question class. Questions no longer spaz.
    It not creates a class and stores the positions then just call the
    getter to blit the information of that question to the screen.

--- prototype.py
+++ prototype.py
@@ -16,6 +16,14 @@ RED = (255, 0, 0)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
+pygame.init()
+
+# Create an 800x600 sized screen
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT], pygame.RESIZABLE)
+
+# Set the title of the window
+pygame.display.set_caption('Prototype')
+
 
 # This class represents the bar at the bottom that the player controls
 class Player(pygame.sprite.Sprite):
@@ -67,9 +75,9 @@ class Player(pygame.sprite.Sprite):
             self.rect.y = self.reset_position_y
             self.rect.x = self.reset_position_x
 
-        block_win_list = pygame.sprite.spritecollide(self, self.end_zone_answer, True)
+        block_win_list = pygame.sprite.spritecollide(self, self.end_zone_answer, False)
         for block in block_win_list:
-            pygame.quit()
+            current_room_no = 1
 
 
 class Wall(pygame.sprite.Sprite):
@@ -111,165 +119,195 @@ class EndZone(pygame.sprite.Sprite):
         self.rect.x = x
 
 
-def question(number):
+class Question:
+    def __init__(self, number):
+        self.question_list = []
+        self.number = number
 
-    # this will open the JSON file, store all the info as list items [x,y,z] in questions,
-    # q will then get question that is past to it
-    with open('questions.json') as f:
-        questions = json.load(f)
-        q = questions[number]  # 0 For getting first question answer set
-        question_text = q["question"]
-        answer_text = q["answer"]
-        fake_one = q["fake1"]
-        fake_two = q["fake2"]
-        fake_three = q["fake3"]
-    f.close()
+        with open('questions.json') as f:
+            questions = json.load(f)
+            q = questions[self.number]  # 0 For getting first question answer set
+            self.question_text = q["question"]
+            self.answer_text = q["answer"]
+            self.fake_one = q["fake1"]
+            self.fake_two = q["fake2"]
+            self.fake_three = q["fake3"]
+        f.close()
 
-    # This is used to position the questions
-    position = {"A": ["A: ", (30, 60)], "B": ["B: ", (30, 120)], "C": ["C: ", (300, 60)], "D": ["D: ", (300, 120)]}
+        self.position = {"A": ["A: ", (30, 60)], "B": ["B: ", (30, 120)], "C": ["C: ", (300, 60)], "D": ["D: ", (300, 120)]}
 
-    #This positions the answers.
-    def position_text(letter, render_text):
-        text = font.render(position[letter][0]+render_text, 1, WHITE)
+        self.font = pygame.font.Font(None, 20)
+
+        self.position_question()
+
+        self.letters = ["A", "B", "C", "D"]
+        random.shuffle(self.letters)  # This makes shuffles the letters so the answers will land on separate questions
+        self.numbers = [1, 2, 3, 4]  # These are used to make the dictionary and will link with the randomised letters
+        self.get_letter = dict(zip(self.numbers, self.letters))  # Make the dictionary
+
+        self.position_text(self.get_letter[1], self.answer_text)  # Print answer to the screen
+
+        self.position_text(self.get_letter[2], self.fake_one)
+
+        self.position_text(self.get_letter[3], self.fake_two)
+
+        self.position_text(self.get_letter[4], self.fake_three)
+
+    def position_text(self, letter, render_text):
+        text = self.font.render(self.position[letter][0]+render_text, 1, WHITE)
         textpos = text.get_rect()
-        textpos.topleft = (position[letter][1])
-        screen.blit(text, textpos)
+        textpos.topleft = (self.position[letter][1])
+        self.question_list.append([text, textpos])
 
-    # This positions the questions.
-    def position_question():
-        text = font.render(question_text, 1, WHITE)
+    def position_question(self):
+        text = self.font.render(self.question_text, 1, WHITE)
         textpos = text.get_rect()
         textpos.centerx = screen.get_rect().centerx
         textpos.centery = 30
-        screen.blit(text, textpos)
+        self.question_list.append([text, textpos])
 
-    font = pygame.font.Font(None, 20) # Load font and font size
-    #Questions
-
-    position_question()  # As the question will be place in same place every time no need for randomisation
-    letters = ["A", "B", "C", "D"]
-    random.shuffle(letters)  # This makes shuffles the letters so the answers will land on separate questions
-    numbers = [1, 2, 3, 4]  # These are used to make the dictionary and will link with the randomised letters
-    get_letter = dict(zip(numbers, letters))  # Make the dictionary
-
-    position_text(get_letter[1], answer_text)  # Print answer to the screen
-
-    position_text(get_letter[2], fake_one)
-
-    position_text(get_letter[3], fake_two)
-
-    position_text(get_letter[4], fake_three)
+    def print_question(self):
+        for i in self.question_list:
+            screen.blit(i[0],i[1])
 
 
+class Map():
+    wall_list = None
 
+    def __init__(self):
+        self.wall_list = pygame.sprite.Group()
 
-# Call this function so the Py game library can initialize itself
-pygame.init()
-
-# Create an 800x600 sized screen
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT], pygame.RESIZABLE)
-
-# Set the title of the window
-pygame.display.set_caption('Prototype')
-
-# List to hold all the sprites
-
-all_sprite_list = pygame.sprite.Group()
-
-wall_list = pygame.sprite.Group()
-
-end_zone_list = pygame.sprite.Group()
-
-end_zone_answer = pygame.sprite.Group()
-
-
-EndZone_to_make = [(70, 720, GREEN), (220, 720, GREEN), (370, 720, GREEN)]
-for i in range(len(EndZone_to_make)):
-    end_zone = EndZone(*EndZone_to_make[i])
-    wall_list.add(end_zone)
-    all_sprite_list.add(end_zone)
-
-EndZone_to_make_answer = EndZone(520, 720, WHITE)
-
-all_sprite_list.add(EndZone_to_make_answer)
-end_zone_answer.add(EndZone_to_make_answer)
-
-def make_walls(list):
-
-    for i in range(len(list)):
-        wall = Wall(*list[i])
-        wall_list.add(wall)
-        all_sprite_list.add(wall)
-
-walls_to_make = [(10, 300, 700, 10, WHITE),
-                 (150, 450, 10, 340, WHITE),
-                 (300, 450, 10, 340, WHITE),
-                 (450, 450, 10, 340, WHITE),
-                 (600, 450, 10, 340, WHITE),
-                 ]
-
-four_sides = [(0, 200, 10, 600, BLUE),
+        four_sides = [(0, 200, 10, 600, BLUE),
               (10, 200, 790, 10, BLUE),
               (0, 790, 800, 10, BLUE),
               (790, 200, 10, 600, BLUE)
               ]
 
-make_walls(walls_to_make)
-make_walls(four_sides)
+        for item in four_sides:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
+
+
+class Map1(Map):
+    def __init__(self):
+        Map.__init__(self)
+
+        walls_to_make = [(10, 300, 700, 10, WHITE),
+             (150, 450, 10, 340, WHITE),
+             (300, 450, 10, 340, WHITE),
+             (450, 450, 10, 340, WHITE),
+             (600, 450, 10, 340, WHITE),
+             ]
+
+        for item in walls_to_make:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
+
+
+class Map2(Map):
+    def __init__(self):
+        Map.__init__(self)
+
+        walls_to_make = [(10, 300, 700, 10, WHITE),
+                        (150, 450, 10, 340, WHITE),
+                        ]
+
+        for item in walls_to_make:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
+
+
+def main():
+    # Call this function so the Py game library can initialize itself
+
+    # List to hold all the sprites
+
+    all_sprite_list = pygame.sprite.Group()
+
+    end_zone_list = pygame.sprite.Group()
+
+    end_zone_answer = pygame.sprite.Group()
+
+    rooms = []
+    room = Map1()
+    rooms.append(room)
+    room = Map2()
+    rooms.append(room)
+
+    current_room_no = 0
+    current_room = rooms[current_room_no]
+
+    current_question_no = 0
+    current_question = Question(current_question_no)
+
+    EndZone_to_make = [(70, 720, GREEN), (220, 720, GREEN), (370, 720, GREEN)]
+    for i in range(len(EndZone_to_make)):
+        end_zone = EndZone(*EndZone_to_make[i])
+        current_room.wall_list.add(end_zone)
+        all_sprite_list.add(end_zone)
+
+    EndZone_to_make_answer = EndZone(520, 720, WHITE)
+
+    all_sprite_list.add(EndZone_to_make_answer)
+    end_zone_answer.add(EndZone_to_make_answer)
+
+    # Create the player paddle object
+    player = Player(250, 50)
+    player.walls = current_room.wall_list
+    player.end_zone_answer = end_zone_answer
+    all_sprite_list.add(player)
 
 
 
-# Create the player paddle object
-player = Player(250, 50)
-player.walls = wall_list
-player.end_zone_answer = end_zone_answer
-all_sprite_list.add(player)
+    clock = pygame.time.Clock()
 
+    done = False
+    x = 1
+    while not done:
 
-clock = pygame.time.Clock()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-done = False
-x = 1
-while not done:
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.changespeed(-3, 0)
+                elif event.key == pygame.K_RIGHT:
+                    player.changespeed(3, 0)
+                elif event.key == pygame.K_UP:
+                    player.changespeed(0, -3)
+                elif event.key == pygame.K_DOWN:
+                    player.changespeed(0, 3)
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.changespeed(3, 0)
+                elif event.key == pygame.K_RIGHT:
+                    player.changespeed(-3, 0)
+                elif event.key == pygame.K_UP:
+                    player.changespeed(0, 3)
+                elif event.key == pygame.K_DOWN:
+                    player.changespeed(0, -3)
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.changespeed(-3, 0)
-            elif event.key == pygame.K_RIGHT:
-                player.changespeed(3, 0)
-            elif event.key == pygame.K_UP:
-                player.changespeed(0, -3)
-            elif event.key == pygame.K_DOWN:
-                player.changespeed(0, 3)
-            elif event.key == pygame.K_ESCAPE:
-                pygame.quit()
+        screen.fill(BLACK)
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player.changespeed(3, 0)
-            elif event.key == pygame.K_RIGHT:
-                player.changespeed(-3, 0)
-            elif event.key == pygame.K_UP:
-                player.changespeed(0, 3)
-            elif event.key == pygame.K_DOWN:
-                player.changespeed(0, -3)
+        current_question.print_question()
 
-    all_sprite_list.update()
+        all_sprite_list.update()
 
-    screen.fill(BLACK)
+        all_sprite_list.draw(screen)
 
-    question(2)
+        current_room.wall_list.draw(screen)
 
+        pygame.display.flip()
 
+        pygame.display.update()
 
-    all_sprite_list.draw(screen)
+        clock.tick(60)
 
-    pygame.display.flip()
+    pygame.quit()
 
-    clock.tick(60)
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
